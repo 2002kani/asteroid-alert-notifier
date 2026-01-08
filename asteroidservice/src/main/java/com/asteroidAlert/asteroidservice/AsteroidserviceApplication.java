0fd362fb56d0cf4dd29bdb12b210b9eb9049db40
02Kani <kanikaradag02@gmail.com>
2026-01-08 22:27:04 +0100

    init commit

---
+++ asteroidservice/src/main/java/com/asteroidAlert/asteroidservice/AsteroidserviceApplication.java
@@ -0,0 +1,13 @@
+package com.asteroidAlert.asteroidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidserviceApplication.class, args);
+	}
+
+}
---
+++ asteroidservice/src/test/java/com/asteroidAlert/asteroidservice/AsteroidserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asteroidAlert.asteroidservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
